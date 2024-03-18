404bfa54c164855c7f8040b15c1727333cc8e8a5
Shaney94 <s1994anderson@gmail.com>
2024-03-18 15:48:56 +0000

    comit

---
+++ leaderboard.js
@@ -0,0 +1,17 @@
+document.getElementById("rank1_Name").innerHTML = "Ryan";
+document.getElementById("rank2_Name").innerHTML = "Shane Anderson";
+document.getElementById("rank3_Name").innerHTML = "Susan";
+document.getElementById("rank4_Name").innerHTML = "Daniel";
+document.getElementById("rank5_Name").innerHTML = "Gina";
+document.getElementById("rank6_Name").innerHTML = "Christopher";
+document.getElementById("rank7_Name").innerHTML = "Latvia";
+document.getElementById("rank8_Name").innerHTML = "George";
+
+document.getElementById("rank1_Time").innerHTML = "02:45";
+document.getElementById("rank2_Time").innerHTML = "02:52";
+document.getElementById("rank3_Time").innerHTML = "03:10";
+document.getElementById("rank4_Time").innerHTML = "03:55";
+document.getElementById("rank5_Time").innerHTML = "03:59";
+document.getElementById("rank6_Time").innerHTML = "04:15";
+document.getElementById("rank7_Time").innerHTML = "04:22";
+document.getElementById("rank8_Time").innerHTML = "07:27";
