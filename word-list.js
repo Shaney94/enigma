a12fd19d33a0a847394a21d8e0ad20c73ce9e587
Shaney94 <s1994anderson@gmail.com>
2024-03-19 11:23:13 +0000

    test

---
+++ word-list.js
@@ -0,0 +1,50 @@
+const wordList = [
+    {
+        word: "blitz",
+        hint: "A sudden, intensive military attack, particularly the London bombings."
+    },
+    {
+        word: "radar",
+        hint: "A detection system that uses radio waves to determine the range, angle, or velocity of objects."
+    },
+    {
+        word: "ally",
+        hint: "A nation that joins another in a common cause."
+    },
+    {
+        word: "axis",
+        hint: "The coalition headed by Germany, Italy, and Japan."
+    },
+    {
+        word: "enigma",
+        hint: "A machine used for the encryption and decryption of secret messages by the Germans."
+    },
+    {
+        word: "patton",
+        hint: "A famous American general known for his leadership in the European theater."
+    },
+    {
+        word: "stalin",
+        hint: "The leader of the Soviet Union during World War II."
+    },
+    {
+        word: "luftwaffe",
+        hint: "The aerial warfare branch of the German Wehrmacht."
+    },
+    {
+        word: "normandy",
+        hint: "The location of the Allied invasion of occupied France in 1944."
+    },
+    {
+        word: "panzer",
+        hint: "A German tank used during World War II"
+    },
+    {
+        word: "gestapo",
+        hint: "The official secret police of Nazi Germany."
+    },
+    {
+        word: "spitfire",
+        hint: "A British fighter aircraft that was used throughout World War II."
+    }
+];
